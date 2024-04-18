11ce1f04f03e0d14cbe47b39075fe9d562086571
Andy <andypicke@gmail.com>
2024-04-18 10:50:17 -0600

    initial commit new package

---
+++ R/fetch_coagmet_data_csv.R
@@ -0,0 +1,21 @@
+#' @title fetch_coagmet_data_csv
+#' @param data_url API url for data request
+#' @description
+#' Fetch csv data from CoAgMet API for specified url
+#' @author Andy Pickering
+#' @returns Data frame of CoAgMet data
+#'
+
+
+
+
+#-------------------------------------
+# function to get data from API
+#-------------------------------------
+fetch_coagmet_data_csv <- function(data_url){
+
+  df <- readr::read_csv(file = data_url,
+                        col_names = TRUE,
+                        na = "-999")  # missing values aremarked as -999 in data
+  return(df)
+}

51670f3f00fcec5c8f02c1006830f15d90755744
Andy <andypicke@gmail.com>
2024-04-21 13:53:25 -0600

    add plotting function

---
+++ R/plot_coagmet_plotly.R
@@ -0,0 +1,25 @@
+#' @title Make plotly plot of one variable
+#' @param df  Dataframe of CoAgMet data
+#' @param var_to_plot Variable to plot
+#' @returns g: Plotly plot object
+#'
+#' @export
+
+
+
+plot_coagmet_plotly <- function(df, var_to_plot="air_temp"){
+
+  df_to_plot <- df |>
+    dplyr::select(date_and_time, all_of(var_to_plot)) |>
+    dplyr::rename(xvar = date_and_time, yvar = var_to_plot)
+
+  g <- plotly::plot_ly(data = df_to_plot, x = ~xvar, y = ~yvar) |>
+    plotly::add_lines(name = var_to_plot) |>
+    plotly::layout(
+      xaxis = list(title = "Date"),
+      yaxis = list(title = var_to_plot)
+    )
+
+  return(g)
+
+}

5cc6e17681fe51d372481420c13758a712852a85
Andy <andypicke@gmail.com>
2024-04-21 14:02:30 -0600

    add title to plot

--- R/plot_coagmet_plotly.R
+++ R/plot_coagmet_plotly.R
@@ -1,13 +1,13 @@
 #' @title Make plotly plot of one variable
-#' @param df  Dataframe of CoAgMet data
-#' @param var_to_plot Variable to plot
+#' @param df  Dataframe of processed CoAgMet data
+#' @param var_to_plot Variable to plot; default is air_temp
 #' @returns g: Plotly plot object
 #'
 #' @export
 
 
 
-plot_coagmet_plotly <- function(df, var_to_plot="air_temp"){
+plot_coagmet_plotly <- function(df, var_to_plot = "air_temp"){
 
   df_to_plot <- df |>
     dplyr::select(date_and_time, all_of(var_to_plot)) |>
@@ -16,6 +16,7 @@ plot_coagmet_plotly <- function(df, var_to_plot="air_temp"){
   g <- plotly::plot_ly(data = df_to_plot, x = ~xvar, y = ~yvar) |>
     plotly::add_lines(name = var_to_plot) |>
     plotly::layout(
+      title = glue::glue('{df$station[1]} : {var_to_plot}'),
       xaxis = list(title = "Date"),
       yaxis = list(title = var_to_plot)
     )

c2e53831fb7389d97c9b65a040c57abdcbc96c49
Andy <andypicke@gmail.com>
2024-04-23 10:14:03 -0600

    add function to find closest station

---
+++ R/find_closest_coagmet_station.R
@@ -0,0 +1,28 @@
+#' @title Find closest CoAgMet station to given location
+#' @param xlat latitidue (degN)
+#' @param xlon longitude (degE)
+#' @returns closest_station: 1 row Dataframe of info for closest station
+#' @export
+#' @author Andy Pickering
+
+
+
+find_closest_coagmet_station <- function(xlat, xlon){
+
+  # get all stations info
+  stations <- rcoagmet::get_coagmet_meta()
+
+  # distances between (xlon,xlat) and stations in meters
+  dists <- geosphere::distHaversine(c(xlon,xlat), dplyr::bind_cols(stations$longitude_deg_e,stations$latitude_deg_n))
+
+  #min(dists)
+  imin <- which.min(dists)
+
+  closest_station <- stations[imin,]
+  closest_station$distance_m <- dists[imin]
+
+  return(closest_station)
+
+}
+
+

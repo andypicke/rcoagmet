173e26c43ab23fc32010bfc9a4ca91fccf5fd931
Andy <andypicke@gmail.com>
2024-04-24 13:23:04 -0600

    add option to get all stations

--- R/construct_data_url.R
+++ R/construct_data_url.R
@@ -1,5 +1,5 @@
 #' @title Construct url request for data
-#' @param station_id ID of weather station; default 'cht01'
+#' @param station_id ID of weather station; default 'cht01'. Use 'all' to get data for all stations
 #' @param time_step Frequency of data (5min, hourly, daily); default 'hourly'
 #' @param date_from Start date of data to get in yyyy-mm-dd format: default is 5 days ago
 #' @param date_to End date of data in yyyy-mm-dd format; defaut 'now'
@@ -13,8 +13,17 @@ construct_data_url <- function(station_id="cht01", time_step="hourly", date_from
 
   date_str <- paste0("from=", date_from, "&to=", date_to)
 
-  data_url <- paste0(base_url, 'data/',time_step ,'/',station_id,
-                     ".csv?header=yes&", date_str,
-                     "&fields=t,rh,dewpt,solarRad,precip,windSpeed,windDir,gustSpeed,gustTime,gustDir,st5cm,st15cm")
+  if (station_id == "all") {
+    data_url <- paste0(base_url, 'data/',time_step ,
+                       ".csv?header=yes&", date_str,
+                       "&fields=t,rh,dewpt,solarRad,precip,windSpeed,windDir,gustSpeed,gustTime,gustDir,st5cm,st15cm")
+
+  } else{
+
+    data_url <- paste0(base_url, 'data/',time_step ,'/',station_id,
+                       ".csv?header=yes&", date_str,
+                       "&fields=t,rh,dewpt,solarRad,precip,windSpeed,windDir,gustSpeed,gustTime,gustDir,st5cm,st15cm")
+
+  }
 
 }

0360e51486a55f4085f7f5fbfc04eba81321c70e
Andy <andypicke@gmail.com>
2024-04-25 14:52:51 -0600

    add option to specify one station in get_coagmet_meta

--- R/get_coagmet_meta.R
+++ R/get_coagmet_meta.R
@@ -1,10 +1,16 @@
 #' @title Get Metadata for CoAgMet Weather Stations
+#' @param station_id ID of station to retrieve info for. Default is all stations
 #' @returns Data frame of metadata for all CoAgMet Weather Stations
 #' @export
-#'
+#' @author Andy Pickering
 
-get_coagmet_meta <- function(){
+get_coagmet_meta <- function(station_id = "all") {
   meta <- readr::read_csv(file = "https://coagmet.colostate.edu/data/metadata.csv?header=yes") |>
     janitor::clean_names()
+
+  if (station_id != "all") {
+    meta <- meta |> dplyr::filter(station == station_id)
+  }
+
   return(meta)
 }

da08921b1d8625a9de6517f1fca0273ed63e1293
Andy <andypicke@gmail.com>
2024-05-05 13:12:44 -0600

    add option to get info for norther water stations also

--- R/get_coagmet_meta.R
+++ R/get_coagmet_meta.R
@@ -1,5 +1,6 @@
 #' @title Get Metadata for CoAgMet Weather Stations
 #' @param station_id ID of station to retrieve info for. Default is all stations
+#' @param network Network :either "coagmet" or "nw" (northern water). Default is coagmet
 #' @returns Data frame of metadata for all CoAgMet Weather Stations
 #' @examples
 #' meta <- get_coagmet_meta()
@@ -7,9 +8,18 @@
 #' @export
 
 
-get_coagmet_meta <- function(station_id = "all") {
+get_coagmet_meta <- function(station_id = "all", network = "coagmet") {
+
+  if (network == "coagmet") {
+    file_url <- "https://coagmet.colostate.edu/data/metadata.csv?header=yes"
+  } else if (network == "nw") {
+    file_url <- "https://coagmet.colostate.edu/data/nw/metadata.csv?header=yes"
+  } else {
+    stop("network must be 'coagmet' or 'nw' ")
+  }
+
   meta <- readr::read_csv(
-    file = "https://coagmet.colostate.edu/data/metadata.csv?header=yes",
+    file = file_url,
     show_col_types = FALSE) |>
     janitor::clean_names()
 

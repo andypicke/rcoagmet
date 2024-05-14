7d68b48a9a47615e9d42fd9dcb6ab03eb15f8ce0
Andy <andypicke@gmail.com>
2024-05-14 10:01:33 -0600

    use httr for API request so we can get status code etc..

--- R/fetch_coagmet_data_csv.R
+++ R/fetch_coagmet_data_csv.R
@@ -11,10 +11,22 @@
 # function to get data from API
 #-------------------------------------
 fetch_coagmet_data_csv <- function(data_url) {
+
+  resp <- httr::GET(data_url, httr::user_agent("rcoagmet (https://github.com/andypicke/rcoagmet)"))
+
+  # check if successful response code returned
+  if (resp$status_code != 200) {
+    stop(paste("API returned not 200 status code: ", resp$status_code))
+  }
+
+  #parse response
+  resp_parsed <- httr::content(resp, as = "text")
+
   df <- readr::read_csv(
-    file = data_url,
+    file = resp_parsed,
     col_names = TRUE,
     na = "-999", # missing values ar emarked as -999 in data
     show_col_types = FALSE
   )
+
 }
